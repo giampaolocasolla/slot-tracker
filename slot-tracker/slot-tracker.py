5c757de4462a87f01b363c3f9aa8fffb8a1f06a5
Giampaolo Casolla <giampaolocasolla@gmail.com>
2020-04-11 22:36:07 +0200

    Add function to find actual value on a screenshot

--- slot-tracker/slot-tracker.py
+++ slot-tracker/slot-tracker.py
@@ -1,1 +1,29 @@
-print("Setup Slot Tracker")
+import re
+
+import pyautogui
+import pytesseract
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+try:
+    from PIL import ImageGrab
+except ImportError:
+    import ImageGrab
+
+
+def actual_value(img=None):
+    if img is None:
+        img = ImageGrab.grab()
+    window = pytesseract.image_to_string(img, lang="ita")
+    denaro = re.findall(r"\bDenaro: \€\d+[.,]\d+\b", window)
+    try:
+        value_gross = denaro[0].split("€")[-1].replace(",", ".")
+    except IndexError:
+        value_gross = None
+    return value_gross
+
+
+print(actual_value(Image.open("../data/prova_slot.png")))
+print(actual_value())
